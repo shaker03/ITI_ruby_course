bbfa89decbaf82a5b803378240579cd59ce26bc1
shaker03 <mohammed.ash0101@gmail.com>
2026-06-07 20:16:09 +0300

    day3

--- bank.rb
+++
@@ -1,5 +0,0 @@
-class Bank
-  def process_transactions(transactions, &block)
-    raise "Method #{__method__} is abstract and should be implemented in the child class"
-  end
-end
--- user.rb
+++
@@ -1,8 +0,0 @@
-class User
-  attr_accessor :name, :balance
-
-  def initialize(name, balance)
-    @name = name
-    @balance = balance
-  end
-end
